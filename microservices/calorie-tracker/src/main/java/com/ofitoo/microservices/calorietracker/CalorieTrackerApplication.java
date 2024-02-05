04401e6af1a9af4802a869a90fb2e050bbc82a0d
mcblankenburg <maciej.blankenburg@gmail.com>
2024-02-05 02:50:17 +0100

    Added starter application class and pom to each microservice.
    Took 20 minutes

---
+++ microservices/calorie-tracker/src/main/java/com/ofitoo/microservices/calorietracker/CalorieTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.ofitoo.microservices.calorietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieTrackerApplication.class, args);
+    }
+}
