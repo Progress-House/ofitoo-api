cee081eb19faf9f48ca8438ca55c85f6adabeab7
mcblankenburg <maciej.blankenburg@gmail.com>
2024-02-05 02:50:17 +0100

    feat: add starter application class and pom to each microservicoo

---
+++ microservices/habit-tracker/src/main/java/com/ofitoo/microservices/habittracker/HabitTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.ofitoo.microservices.habittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HabitTrackerApplication.class, args);
+    }
+}
