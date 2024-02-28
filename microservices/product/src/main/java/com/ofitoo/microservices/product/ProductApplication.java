3d0e333425c77a5ec4072ae3031bd3c4f25e0dc6
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2024-02-28 18:45:50 +0100

    Implement POST endpoint

--- microservices/product/src/main/java/com/ofitoo/microservices/product/ProductApplication.java
+++ microservices/product/src/main/java/com/ofitoo/microservices/product/ProductApplication.java
@@ -1,11 +1,7 @@
 package com.ofitoo.microservices.product;
 
-import com.ofitoo.microservices.product.entity.ProductEntity;
-import com.ofitoo.microservices.product.repository.ProductRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductApplication {
--- microservices/product/src/main/java/com/ofitoo/microservices/product/controller/ProductController.java
+++ microservices/product/src/main/java/com/ofitoo/microservices/product/controller/ProductController.java
@@ -2,9 +2,7 @@ package com.ofitoo.microservices.product.controller;
 
 import com.ofitoo.microservices.product.dto.CreateProductDto;
 import com.ofitoo.microservices.product.dto.ProductDto;
-import com.ofitoo.microservices.product.enums.Visibility;
 import com.ofitoo.microservices.product.service.ProductService;
-import lombok.Generated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,8 +15,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public ResponseEntity<ProductDto> addProduct(@RequestBody final CreateProductDto createProductDto) {
-        final Long ownerId = 12L; //todo establish form where get it
+    public ResponseEntity<ProductDto> addProduct(@RequestBody final CreateProductDto createProductDto, @RequestHeader("ownerId") Long ownerId) {
         final var product = productService.addProduct(createProductDto, ownerId);
 
         return ResponseEntity.ok().body(product);
