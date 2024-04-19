8dfa7d15b776a560665d5e75961885e3946706db
mcblankenburg <maciej.blankenburg@gmail.com>
2024-04-20 01:19:11 +0200

    feat: added springdoc-openapi-starter-webmvc-ui for product API

--- microservices/product/src/main/java/com/ofitoo/microservices/product/ProductApplication.java
+++ microservices/product/src/main/java/com/ofitoo/microservices/product/ProductApplication.java
@@ -1,8 +1,11 @@
 package com.ofitoo.microservices.product;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Product API", version = "0.1", description = "API for product service of ofitoo"))
 @SpringBootApplication
 public class ProductApplication {
     public static void main(String[] args) {
