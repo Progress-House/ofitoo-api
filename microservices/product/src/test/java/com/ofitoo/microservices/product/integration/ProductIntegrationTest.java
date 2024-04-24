87c44dbbc1b3dd061076ea0633b2fb498f4cc902
KrystynaCal <krystynacal5@gmail.com>
2024-04-24 21:59:36 +0200

    Add test shouldReturnOwnedProductsByBarcode

--- microservices/product/src/test/java/com/ofitoo/microservices/product/integration/ProductIntegrationTest.java
+++ microservices/product/src/test/java/com/ofitoo/microservices/product/integration/ProductIntegrationTest.java
@@ -7,6 +7,8 @@ import com.ofitoo.microservices.product.model.dto.ProductDto;
 import com.ofitoo.microservices.product.utils.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
@@ -38,4 +40,34 @@ public class ProductIntegrationTest extends BaseIntegrationTest {
                 .ignoringFields("id")
                 .isEqualTo(expectedProductDto);
     }
-}
+
+    @Test
+    public void shouldReturnOwnedProductsByBarcode(){
+
+        // given
+        final CreateProductDto createProductDto = CreateProductDtoModelTestUtil.basic();
+
+        given()
+                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .header(USER_ID, "123")
+                .body(createProductDto)
+                .when()
+                .post(BASE_PATH)
+                .then()
+                .statusCode(200);
+
+        // when
+        final List<ProductDto> productDtos = given()
+                .header(USER_ID, "123")
+                .param("barcode", "123456789")
+                .when()
+                .get(BASE_PATH)
+                .then()
+                .statusCode(200)
+                .extract().jsonPath().getList(".", ProductDto.class);
+        // then
+        assertThat(productDtos).isNotEmpty();
+        for (ProductDto productDto : productDtos) {
+            assertThat(productDto.barcode()).isEqualTo("123456789");
+    }
+}}
